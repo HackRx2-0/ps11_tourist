a7727b2c2c0f79d35ea242fe1893a2c4542d42a7
jindal2209 <shubham7811@yahoo.com>
2021-07-23 14:16:03 +0530

    index.js backend

---
+++ backend/models.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+const jwt = require('jsonwebtoken')
+
+
+// Base schema
+
+
+// LoginUser schema
+
+
+// message schema
+
+
+// prescription schema
+
+
+// payment schema
+
+
+
+// exports
